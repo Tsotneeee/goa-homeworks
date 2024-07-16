6329eecf2e7eae7b0ec2e6d4718f62e565c43b08
tsotneeee <tsotnebajelidze@gmail.com>
2024-07-16 13:37:09 +0400

    seventh commit

---
+++ day44/homework/homework.py
@@ -0,0 +1,104 @@
+# 1
+print("hello world") #displays 'hello world' in teminal
+
+# 2
+print(input()) # displays user's input in terminal
+
+
+# 3
+
+
+
+# 4
+if 6 == 6:
+    print("6 = 6") 
+
+# 5
+
+
+
+
+# 1
+integer = 6
+string = "hello world"
+float3 = 2.5
+boolen = True
+
+# 2
+
+
+
+
+# 3
+x = input() 
+print(x)
+
+
+# 1
+integer = 6
+string = "hello world"
+float4 = 2.5
+boolen = True
+
+# 2
+convert1 = str(integer)
+convert2 = float(integer)
+
+# 3
+var1 = "hello world"
+print(type(var1))
+
+# 4
+
+# python numbers
+
+# 1
+print(5 + 9)
+
+# 2
+
+
+
+# 3
+num = 5
+print( num**2 )
+
+
+# 4
+
+
+
+
+# python casting
+
+# 1
+integer2 = 6
+float1 = float(integer2)
+
+
+# 2
+print(int("5") + float("6"))
+
+
+
+# 3
+
+
+# 4
+
+
+
+# 5
+print(int("3") + float("8"))
+
+
+# boolens
+
+# 1
+if 5 > 2:
+    print("yes")
+
+# 2
+if 4 < 8 and 6 > 3 or 12 >= 4:
+    print("true")
+
